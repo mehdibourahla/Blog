d471edc2a29df1c1739cb7bad1f52203a9053295
Mehdi Bourahla <mehdibrhl4@gmail.com>
2019-08-29 22:36:00 +0100

    Edit env IP

--- app.js
+++ app.js
@@ -50,6 +50,6 @@ app.use(indexRoutes);
 app.use("/blogs",blogRoutes);
 app.use("/blogs/:blogid/comments",commentRoutes);
 
-app.listen(process.env.PORT,function(){
+app.listen(process.env.PORT,process.env.IP,function(){
     console.log("SERVER ON");
 }); 
